fb605ad71b2aa5f5c4798e23fb1bda821db4fd15
parkjunghun <go9018@naver.com>
2019-05-13 19:34:18 +0900

    asynctask use rxandroid example add

--- rxandroid_sample1/src/main/java/com/example/rxandroid_sample1/activities/AsyncTask_Activity.java
+++ rxandroid_sample1/src/main/java/com/example/rxandroid_sample1/activities/AsyncTask_Activity.java
@@ -2,6 +2,7 @@ package com.example.rxandroid_sample1.activities;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.rxandroid_sample1.R;
@@ -9,21 +10,38 @@ import com.example.rxandroid_sample1.R;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import io.reactivex.MaybeObserver;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 
 public class AsyncTask_Activity extends AppCompatActivity {
+    private static final String TAG = "AsyncTask_Activity";
     private MyTask myTask;
 
     @BindView(R.id.async_test_txtview)
     TextView asynctask_txtview;
+    @BindView(R.id.rxandroid_async_txtview)
+    TextView rxTextView;
+
+    private Unbinder unbinder;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async_task);
-        ButterKnife.bind(this);
+        unbinder = ButterKnife.bind(this);
 
         initAndroidAsync();
+        initRxAsync();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 
     private void initAndroidAsync(){
@@ -47,6 +65,42 @@ public class AsyncTask_Activity extends AppCompatActivity {
             super.onPostExecute(s);
             asynctask_txtview.setText(s);
         }
+    }
+
+    private void initRxAsync() {
+        Observable.just("Hello", "rx", "world")
+                .reduce((x,y) -> x + " " + y)
+                .observeOn(AndroidSchedulers.mainThread())
+                //.subscribe(getObserver());
+                .subscribe(
+                        rxTextView::setText,
+                        e -> Log.e(TAG, e.getMessage()),
+                        () -> Log.i(TAG, "done")
+                );
+
+    }
+
+
+    private MaybeObserver<String> getObserver() {
+        return new MaybeObserver<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {}
+
+            @Override
+            public void onSuccess(String result) {
+                rxTextView.setText(result);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "done");
+            }
+        };
     }
 }
 
