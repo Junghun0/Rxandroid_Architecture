3cb19d2adb5baec496aca9ba8d7f9b31bc2352ee
parkjunghoon <go9018@naver.com>
2019-05-16 23:15:55 +0900

    test commit

--- todoapp_mvp/src/main/java/com/example/todoapp_mvp/MainActivity.java
+++ todoapp_mvp/src/main/java/com/example/todoapp_mvp/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
